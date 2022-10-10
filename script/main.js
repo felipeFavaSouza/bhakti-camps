1a3b688412ebc2d218f3f38f19eabc1a5f44e77b
felipeSouza <felipefavasouza@gmail.com>
2022-10-10 10:21:16 -0300

    Improving the logic with classes and arrays

--- script/main.js
+++ script/main.js
@@ -1,30 +1,87 @@
-// // Calculadora de costo x cantidad de hijos y metodo de pago. 
+// Formulario registro de participante en campamento. 
 
-// const campCost = 2000;
-// let totalCost = 0;
-// let paymentType = prompt('Por favor elija un metodo de pago. "tc" para tarjeta de credito, "ef" para efectivo, "tf" para transferencia o "salir" para cerrar la calculadora');
+const allParticipants = []; 
+
+class Participant {
+    constructor(name, age, country, city, paymentType, transportationAssist){
+        this.name = name;
+        this.age = age;
+        this.country = country;
+        this.city = city;
+        this.paymentType = paymentType;
+        this.transportationAssist = transportationAssist; 
+    }
+    viewParticipant() {
+        if(this.transportationAssist == 'si'){
+            alert(`Recibimos el registro de Nombre: ${this.name} Edad: ${this.age} Pais: ${this.country} Ciudad: ${this.city} Metodo Pago: ${this.paymentType}. Nuestro Equipo va ayudarlos con el transporte`); 
+        } else {
+            alert(`Recibimos el registro de Nombre: ${this.name} Edad: ${this.age} Pais: ${this.country} Ciudad: ${this.city} Metodo Pago: ${this.paymentType}. Los padres se hacen cargo del transporte`);
+        }
+    }
+    calculateTotal(){
+        let totalCost = 0;
+        if(this.transportationAssist == 'si' && this.paymentType == 'tc'){
+            totalCost = `El costo total es $${campCost + transportServices * 1.15} incluye el recargo de 15% por pago con TC`; 
+        }else if(this.transportationAssist == 'si' && this.paymentType == 'td') {
+            totalCost = `El costo total es $${campCost + transportServices}`; 
+        }else if(this.transportationAssist == 'si' && this.paymentType == 'tr') {
+            totalCost = `El costo total es $${campCost + transportServices}`; 
+        }else if(this.transportationAssist == 'no' && this.paymentType == 'tc') {
+            totalCost = `El costo total es $${campCost * 1.15} incluye el recargo de 15% por pago con TC`; 
+        }else {
+            totalCost = campCost; 
+        }
+        return totalCost;
+    }
+}
 
 
-// const calculateTotal = () => {
-//     let childQuantity = parseInt(prompt('Cuantos hijos te gustaria enviar al campamento?'));
-//     if(childQuantity === 2) {
-//         totalCost = (campCost * 2) * 0.9;
-//         alert(`Enviado 2 hijos obtendras un descuento de 10% llevando el costo total a $ ${totalCost}`);
-//         return totalCost;
-//     } else if(childQuantity >= 3){
-//         totalCost = (campCost * childQuantity) * 0.85;
-//         alert(`Enviado ${childQuantity} hijos obtendras un descuento de 15% llevando el costo total a $ ${totalCost}`);
-//         return totalCost;
-//     }
+const campCost = 2000;
+const transportServices = 1000;
+let initialInput = prompt('Hola! Te gustaria registrar a tu Hijo/a al campamento? "Si" o "No"? Para salir del formulario escribi "Salir". Para saber la cantidad de participantes indica "Cantidad"').toLowerCase();
 
-// }
+const newParticipant = () => {
+    let nombre = prompt('Indiquenos el nombre del participante').toLowerCase();
+    let edad = parseInt(prompt('Indiquenos la edad del participante')); 
+    let pais = prompt('Indiquenos el pais del participante').toLowerCase();
+    let ciudad = prompt('Indiquenos la ciudad del participante').toLowerCase();
+    let metodoPago = prompt('Como hara el pago de la inscripcion? Para Tarjeta de Credito escriba "TC", para debito "TD" y transferencia "TR"').toLowerCase();
+    let transporte = prompt('El/La Participante va necesitar ayuda en el transporte?').toLowerCase();
 
-// while(paymentType != 'salir'){
-//     if(paymentType == 'ef' || paymentType == 'tf'){
-//         calculateTotal();
-//     } else {
-//         alert('En este caso no contas con descuento');
-//     }
-//     paymentType = prompt('Por favor elija un metodo de pago. "tc" para tarjeta de credito, "ef" para efectivo, "tf" para transferencia o "salir" para cerrar la calculadora');
-// }
+    let nuevoParticipante = new Participant(nombre, edad, pais, ciudad, metodoPago, transporte);
+    allParticipants.push(nuevoParticipante);
+
+    return nuevoParticipante; 
+}
+
+const calculateTotal = () => {
+    if(participante.transportationAssist == 'si' && participante.paymentType == 'tc'){
+        totalCost = campCost + transportServices * 1.15; 
+    }else if(participante.transportationAssist == 'si' && participante.paymentType == 'td') {
+        totalCost = campCost + transportServices; 
+    }else if(participante.transportationAssist == 'si' && participante.paymentType == 'tr') {
+        totalCost = campCost + transportServices; 
+    }else if(participante.transportationAssist == 'no' && participante.paymentType == 'tc') {
+        totalCost = campCost * 1.15; 
+    }else {
+        totalCost = campCost; 
+    }
+}
+
+
+while(initialInput != 'salir'){
+    if(initialInput == 'cantidad'){
+        alert(`Ya tenemos a ${allParticipants.length} inscriptos`)
+    }else if (initialInput == 'si'){
+        let secondInput = prompt(`El costo por cada participante es de $${campCost} y el Transporte cuesta $${transportServices}. Desea continuar? "Si" o "No"`).toLowerCase();
+        if(secondInput == 'si'){
+            let participante = newParticipant();
+            alert(participante.viewParticipant());
+            alert(participante.calculateTotal()); 
+        }
+    }else {
+        alert('Es una pena no tenerlos en este camping');
+    }
+    initialInput = prompt('Hola! Te gustaria registrar a tu Hijo/a al campamento? "Si" o "No"? Para salir del formulario escribi "Salir". Para saber la cantidad de participantes indica "Cantidad"').toLowerCase();
+}
 
