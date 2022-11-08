93c1d56097d0f992700c665e8d39ced90c854e7b
felipeSouza <felipefavasouza@gmail.com>
2022-11-08 00:46:21 -0300

    adding validation to payment and alert

--- script/confirmacion.js
+++ script/confirmacion.js
@@ -7,6 +7,10 @@ const cancelarBtn = document.querySelector("#cancel-btn");
 const costoTotal = document.querySelector("#valor-total");
 const paymentCost = document.querySelector("#paymentCost");
 const botonPagar = document.querySelector("#paymentBtn");
+const paymentForm = document.querySelector("#payment-form");
+const paymentName = document.querySelector("#paymentName")
+const cardNum = document.querySelector("#cardNum")
+let validar = 0;
 
 //Funcion que calcula total a pagar
 
@@ -43,18 +47,78 @@ listaParticipantes.forEach(participante => {
     paymentCost.innerHTML = `<p class="valor-confirmacion">${calculateTotal()}</p>`
 })
 
+// Funcion que agrega mensaje de error si el usuario no pone sus datos en input
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add("error");
+    inputControl.classList.remove('success');
+};
+
+// Funcion que agrega clase success a los campos correctos
+
+const setSuccess = (element) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add("success");
+    inputControl.classList.remove('error');
+    validar ++;
+};
+
+
+//funcion de validacion de datos 
+
+const validateInputs = () => {
+    const nombre = paymentName.value.trim();
+    const numeroTarjeta = parseInt(cardNum.value.trim());
+
+    if(nombre == ''){
+        setError(paymentName, 'Nombre completo es obligatorio');
+    } else {
+        setSuccess(paymentName);
+    }
+
+    if(numeroTarjeta == ''){
+        setError(cardNum, 'Edad es obligatoria');
+    }else if(numeroTarjeta < 16) {
+        setError(cardNum, 'Numero invalido, debe contener 16 digitos');
+    }else if(numeroTarjeta > 16) {
+        setError(cardNum, 'Numero invalido, debe contener 16 digitos')
+    }
+    else {
+        setSuccess(cardNum);
+    }
+
+}
+
 
 //EVENTOS
-// boton de confirmacion que genera alert de sweet alert
+// una vez enviado en form primero aparece un alert y luego se envia los datos.
 
-botonPagar.addEventListener("click", () => {
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Pago Realizado',
-        showConfirmButton: false,
-        timer: 1500
-    });
+paymentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateInputs();
+    if(validar == 2){
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Pago Realizado',
+            showConfirmButton: false,
+            timer: 1500
+        });
+        setTimeout(() => {
+            paymentForm.submit();
+        }, 1800);
+    }else {
+        validar = 0;
+    }
+
+  
 })
 
 // Boton para cancelar el envio de los datos que lleva de vuelta a la pagina del formulario y saca los datos del localStorage.
@@ -63,3 +127,4 @@ cancelarBtn.addEventListener("click", () => {
     localStorage.removeItem("participante");
     listaParticipantes.pop()
 })
+
